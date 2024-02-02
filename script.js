3d7b3c5a9ea59436280b126541ea950b9809c5c2
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-02-01 22:25:00 -0300

    style :gem: estilizando responsividade

--- script.js
+++ script.js
@@ -4,103 +4,129 @@ const cardName = document.querySelector('.cardName');
 const imgList = document.querySelector('.imgList');
 const imgPreview = document.querySelector('.imgPreview');
 const englishName = document.querySelector('.englishName');
-const heartButton = document.querySelector(".heartButton");
-const colorDetails = document.querySelector(".colorDetails");
-const manaDetails = document.querySelector(".manaDetails");
-const cardType = document.querySelector(".cardType");
-
+const heartButton = document.querySelector('.heartButton');
+const colorDetails = document.querySelector('.colorDetails');
+const manaDetails = document.querySelector('.manaDetails');
+const cardType = document.querySelector('.cardType');
+const nameMagica = 'Mágica Instantânea';
 let liked = false;
-
+const SEVEN_SIX_EIGTH = 768;
 const cardSet = [
- {imageUrl: "img/card2.jpg", englishName: "Counterspell", name: "Contramágica_2", mana: 2, color: "Azul", manaImage: "img/blueMana.png", type: "Mágica Instantânea"},
- {imageUrl: "img/card4.jpg",englishName: "Coordinated Assault", name: "Assalto Coordenado", mana:1, color: "Vermelho",manaImage: "img/redMana.png", type: "Instantânea"},
- {imageUrl: "img/card1.jpg", englishName: "Counterspell", mana: 2, name: "Contramágica_1", color: "Azul", type: "Mágica Instantânea"},
- {imageUrl: "img/card3.jpg", englishName: "Counterspell", mana: 2, name: "Contramágica_3", color: "Azul", type: "Mágica Instantânea"},
+  {
+    imageUrl: 'img/card2.jpg',
+    englishName: 'Counterspell',
+    name: 'Contramágica_2',
+    mana: 2,
+    color: 'Azul',
+    manaImage: 'img/blueMana.png',
+    type: nameMagica },
+  {
+    imageUrl: 'img/card4.jpg',
+    englishName: 'Coordinated Assault',
+    name: 'Assalto Coordenado',
+    mana: 1,
+    color: 'Vermelho',
+    manaImage: 'img/redMana.png',
+    type: 'Instantânea' },
+  {
+    imageUrl: 'img/card1.jpg',
+    englishName: 'Counterspell',
+    mana: 2,
+    name: 'Contramágica_1',
+    color: 'Azul',
+    type: nameMagica },
+  {
+    imageUrl: 'img/card3.jpg',
+    englishName: 'Counterspell',
+    mana: 2,
+    name: 'Contramágica_3',
+    color: 'Azul',
+    type: nameMagica },
 ];
 
 navButton.addEventListener('click', () => {
-    if (navContainer.style.display === 'flex') {
-         navContainer.style.display= 'none'
-        } else { 
-            navContainer.style.display= 'flex'
-    };
-})
+  if (navContainer.style.display === 'flex') {
+    navContainer.style.display = 'none';
+  } else {
+    navContainer.style.display = 'flex';
+  }
+});
 
-const adjustMenuOnResize = () =>{
-    if (window.innerWidth >= 768) {
-        navContainer.style.display = 'flex';
-    } else {
-        navContainer.style.display = '';
-    }
-}
+const adjustMenuOnResize = () => {
+  if (window.innerWidth >= SEVEN_SIX_EIGTH) {
+    navContainer.style.display = 'flex';
+  } else {
+    navContainer.style.display = '';
+  }
+};
 
 const addManaImg = (number, url, color) => {
-    manaDetails.innerHTML = "";
-    for (i = 0; i < number; i++ ){
-        manaDetails.innerHTML += `<li><img src="${url}" alt="${color}" class="miniMana"/></li>`;
-    }
-}
+  manaDetails.innerHTML = '';
+  for (i = 0; i < number; i++) {
+    manaDetails
+      .innerHTML += `<li><img src="${url}" alt="${color}" class="miniMana"/></li>`;
+  }
+};
 
 const initialSet = () => {
-    cardName.innerHTML = cardSet[0].name.replace(/_[0-9]+$/, '');
-    englishName.innerHTML = cardSet[0].englishName;
-    addManaImg(cardSet[0].mana, cardSet[0].manaImage, cardSet[0].color)
-    colorDetails.innerHTML = cardSet[0].color;
-    colorDetails.innerHTML = cardSet[0].color;
-    cardType.innerHTML = cardSet[0].type;
-}
+  cardName.innerHTML = cardSet[0].name.replace(/_[0-9]+$/, '');
+  englishName.innerHTML = cardSet[0].englishName;
+  addManaImg(cardSet[0].mana, cardSet[0].manaImage, cardSet[0].color);
+  colorDetails.innerHTML = cardSet[0].color;
+  colorDetails.innerHTML = cardSet[0].color;
+  cardType.innerHTML = cardSet[0].type;
+};
 
-const createListImg = (cardSet) => {
-    imgList.innerHTML = '';
+const createListImg = (cardSet2) => {
+  imgList.innerHTML = '';
 
-    cardSet.forEach(card => {
-        imgList.innerHTML += `<li><img src="${card.imageUrl}" alt="${card.name}" class="miniCard"/></li>`;
-    });
-}
-
+  cardSet2.forEach((card) => {
+    imgList.innerHTML += `<li><img src="${card.imageUrl}" 
+    alt="${card.name}" class="miniCard"/></li>`;
+  });
+};
 
 const clickImage = (event) => {
-    const image = event.target;
-    const miniCard = document.querySelectorAll('.miniCard');
+  const image = event.target;
+  const miniCard = document.querySelectorAll('.miniCard');
 
-    let coloredImage = null;
-    miniCard.forEach(img => {
-        if (img.style.filter === 'none') {
-            coloredImage = img;
-        }
-    });
+  let coloredImage = null;
+  miniCard.forEach((img) => {
+    if (img.style.filter === 'none') {
+      coloredImage = img;
+    }
+  });
 
-    if (coloredImage && coloredImage !== image) {
-        coloredImage.style.filter = 'grayscale(100%)';
-    }
-    image.style.filter = 'none';
-    const cardObject = cardSet.find((card) => card.name === image.alt);
-    const editedTittle = cardObject.name.replace(/_[0-9]+$/, '');
-    cardName.innerHTML = editedTittle;
-    imgPreview.src = cardObject.imageUrl;
-    addManaImg(cardObject.mana, cardObject.manaImage, cardObject.color);
-    colorDetails.innerHTML = cardObject.color;
-    if (cardObject.color === "Azul") {
-      colorDetails.classList = "colorDetails blue";
-    } else {
-        colorDetails.className = "colorDetails red";
-    }
-    englishName.innerHTML = cardObject.englishName;
-    cardType.innerHTML = cardObject.type;
-}
+  if (coloredImage && coloredImage !== image) {
+    coloredImage.style.filter = 'grayscale(100%)';
+  }
+  image.style.filter = 'none';
+  const cardObject = cardSet.find((card) => card.name === image.alt);
+  const editedTittle = cardObject.name.replace(/_[0-9]+$/, '');
+  cardName.innerHTML = editedTittle;
+  imgPreview.src = cardObject.imageUrl;
+  addManaImg(cardObject.mana, cardObject.manaImage, cardObject.color);
+  colorDetails.innerHTML = cardObject.color;
+  if (cardObject.color === 'Azul') {
+    colorDetails.classList = 'colorDetails blue';
+  } else {
+    colorDetails.className = 'colorDetails red';
+  }
+  englishName.innerHTML = cardObject.englishName;
+  cardType.innerHTML = cardObject.type;
+};
 
-function toggleHeart() {
-    liked = !liked;
-    if (liked) {
-        heartButton.innerHTML = "🧡";
-    } else {
-        heartButton.innerHTML = "♡";
-    }
-}
+const toggleHeart = () => {
+  liked = !liked;
+  if (liked) {
+    heartButton.innerHTML = '🧡';
+  } else {
+    heartButton.innerHTML = '♡';
+  }
+};
 
 window.addEventListener('resize', adjustMenuOnResize);
 imgList.addEventListener('click', clickImage);
-
 
 adjustMenuOnResize();
 initialSet();
